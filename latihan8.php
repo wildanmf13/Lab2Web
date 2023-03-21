608a8efffe249dcbab08d06185c115d5dbe36573
wildanmf13 <127379846+wildanmf13@users.noreply.github.com>
2023-03-21 09:13:03 +0700

    Add files via upload

---
+++ latihan8.php
@@ -0,0 +1,8 @@
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+while ($i<=10) {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+}
+?>
---
+++ latihan9.php
@@ -0,0 +1,8 @@
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+do {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+} while ($i<=10);
+?>
